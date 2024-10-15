0666f126c662171a8c90eaaae879d33f44ddf0fe
Irfan Ajmeri <iajmeri43@gmail.com>
2024-10-15 20:21:15 +0530

    Initial Commit

---
+++ DemoAssignmentIA/DemoAssignmentIA/CallManager/CallManager.swift
@@ -0,0 +1,146 @@
+//
+//  CallManager.swift
+//  DemoAssignmentIA
+//
+//  Created by IA on 14/10/24.
+//
+
+import UIKit
+import CallKit
+
+class CallManager: NSObject {
+    
+    static let shared = CallManager()
+    private var provider: CXProvider!
+    private var callController: CXCallController!
+    private var callUUID: UUID?
+    private var callCXHandle: CXHandle?
+    
+    // MARK: - init
+    override init() {
+        super.init()
+        setupProvider()
+        callController = CXCallController()
+    }
+    
+    private func setupProvider() {
+        let providerConfiguration = CXProviderConfiguration(localizedName: "DemoAssignmentIA")
+        providerConfiguration.supportsVideo = true
+        providerConfiguration.maximumCallGroups = 1
+        providerConfiguration.maximumCallsPerCallGroup = 1
+        providerConfiguration.supportedHandleTypes = [.phoneNumber]
+        provider = CXProvider(configuration: providerConfiguration)
+        provider.setDelegate(self, queue: nil)
+    }
+    
+    func reportIncomingCall(uuid: UUID, handle: String, hasVideo: Bool) {
+        callUUID = uuid
+        callCXHandle = CXHandle(type: .phoneNumber, value: handle)
+        
+        let callUpdate = CXCallUpdate()
+        callUpdate.remoteHandle = callCXHandle
+        callUpdate.hasVideo = hasVideo
+        
+        provider.reportNewIncomingCall(with: uuid, update: callUpdate) { error in
+            if let error = error {
+                print("Error reporting incoming call: \(error)")
+            } else {
+                self.handleIncomingCallNotification()
+            }
+        }
+    }
+    
+    private func handleIncomingCallNotification() {
+        if UIApplication.shared.applicationState == .active {
+            // App is in foreground - show custom incoming call UI
+            showIncomingCallUI()
+        } else {
+            // App is in background or locked - CallKit will show full-screen UI
+        }
+    }
+    
+    private func showIncomingCallUI() {
+        let callerName = callCXHandle?.value ?? "Unknown"
+        let incomingCallController = IncomingCallController(callerName: callerName)
+        
+        if let topVC = UIApplication.shared.windows.first?.rootViewController {
+            // check if the IncomingCallController is already presented
+            if topVC.presentedViewController is IncomingCallController {
+                print("Incoming call UI is already presented.")
+                return
+            }
+            if let presentedVC = topVC.presentedViewController as? IncomingCallController {
+                presentedVC.dismiss(animated: false) {
+                    topVC.present(incomingCallController, animated: true, completion: nil)
+                }
+            } else {
+                topVC.present(incomingCallController, animated: true, completion: nil)
+            }
+        }
+    }
+    
+    func handleAcceptCall() {
+        guard let uuid = callUUID else { return }
+        let acceptCallAction = CXAnswerCallAction(call: uuid)
+        let transaction = CXTransaction(action: acceptCallAction)
+        
+        callController.request(transaction) { error in
+            if let error = error {
+                print("Error accepting call: \(error.localizedDescription)")
+            } else {
+                print("Call accepted successfully.")
+            }
+        }
+    }
+    
+    func handleEndCall() {
+        guard let uuid = callUUID else { return }
+        let endCallAction = CXEndCallAction(call: uuid)
+        let transaction = CXTransaction(action: endCallAction)
+        
+        callController.request(transaction) { error in
+            if let error = error {
+                print("Error ending call: \(error.localizedDescription)")
+            } else {
+                print("Call ended successfully.")
+                self.callUUID = nil
+                self.callCXHandle = nil
+            }
+        }
+    }
+    
+    private func getCurrentCallUUID() -> UUID? {
+        return callUUID
+    }
+    
+    private func getCurrentCallCXHandle() -> CXHandle? {
+        return callCXHandle
+    }
+    
+    private func getProvider() -> CXProvider {
+        return provider
+    }
+    
+    private func getCallController() -> CXCallController {
+        return callController
+    }
+}
+//
+// MARK: - CXProviderDelegate
+//
+extension CallManager: CXProviderDelegate {
+    
+    func provider(_ provider: CXProvider, perform action: CXStartCallAction) {
+        action.fulfill()
+        print("Started call")
+    }
+    
+    func provider(_ provider: CXProvider, perform action: CXEndCallAction) {
+        handleEndCall()
+        action.fulfill()
+    }
+    
+    func providerDidReset(_ provider: CXProvider) {
+        print("Provider reset")
+    }
+}
---
+++ DemoAssignmentIA/DemoAssignmentIA/Helper/CustomViews.swift
@@ -0,0 +1,96 @@
+//
+//  CustomViews.swift
+//  DemoAssignmentIA
+//
+//  Created by IA on 15/10/24.
+//
+
+import UIKit
+//
+// MARK: - TitleLabel
+//
+class TitleLabel: UILabel {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        translatesAutoresizingMaskIntoConstraints = false
+        backgroundColor = .clear
+        textColor = .black
+        font = .systemFont(ofSize: 18)
+        textAlignment = .center
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+//
+// MARK: - CustomButton
+//
+class CustomButton: UIButton {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        translatesAutoresizingMaskIntoConstraints = false
+        backgroundColor = .systemBlue
+        setTitleColor(.white, for: .normal)
+        layer.cornerRadius = 5.0
+        clipsToBounds = true
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+//
+// MARK: - RoundButtonView
+//
+class RoundButtonView: UIView {
+    
+    // MARK: - create UI
+    let bottomLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .darkGray
+        label.font = .systemFont(ofSize: 16)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    let roundButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.tintColor = .white
+        button.layer.cornerRadius = 40
+        button.clipsToBounds = true
+        return button
+    }()
+    
+    // MARK: - init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    private func setupUI() {
+        translatesAutoresizingMaskIntoConstraints = false
+        addSubview(bottomLabel)
+        addSubview(roundButton)
+        // Layout Constraint
+        NSLayoutConstraint.activate([
+            bottomLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -10),
+            bottomLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
+            bottomLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10),
+            bottomLabel.heightAnchor.constraint(equalToConstant: 20),
+            
+            roundButton.bottomAnchor.constraint(equalTo: bottomLabel.topAnchor, constant: -10),
+            roundButton.centerXAnchor.constraint(equalTo: centerXAnchor),
+            roundButton.widthAnchor.constraint(equalToConstant: 80),
+            roundButton.heightAnchor.constraint(equalToConstant: 80),
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
